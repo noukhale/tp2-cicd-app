7e325755d96df0d0b0b44d158c08b2540690bc18
noukhale <abdel.noukhale@gmail.com>
2025-09-27 14:00:17 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 app.get('/', (req, res) => {
-res.send('Hello CI/CD with GitHub and Azure Container Apps!');
+res.send('Hello CI/CD with GitHub and Azure Container Apps2!');
 });
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
